d8158c09e20635b4df13507ca4b803a6a69a6541
Dmitry Zadnepryansky <d.zadnepryansky@gmail.com>
2023-01-07 21:48:07 +0200

    added some examples

---
+++ pytest_examples/test_script.py
@@ -0,0 +1,18 @@
+# import pytest
+from func_test import our_func
+
+
+def test_funct_2():
+    assert our_func(1, 2, 3) != 3
+
+
+def test_funct_3():
+    assert our_func(5, 6, 2) == 12
+
+
+def test_funct_4():
+    assert our_func(5, 5, 2) == 2
+
+
+def test_funct_1():
+    assert our_func(1, 2, 3) == 6

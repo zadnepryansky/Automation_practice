785bba7eddd8c84284b3f533ff03f3f32c42fa3b
zadnepryansky <d.zadnepryansky@gmail.com>
2023-01-09 20:24:45 +0200

    added selenium

---
+++ selenium_examples/script.py
@@ -0,0 +1,14 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+
+driver.get('https://google.com')
+element_path = ''
+time.sleep(100)
+# driver.quit()

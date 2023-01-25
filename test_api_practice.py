044501137f4363dbaa44a79629de0449366f4587
Dmitry Zadnepryansky <d.zadnepryansky@gmail.com>
2023-01-25 18:56:28 +0200

    added a new lessons at Hillel IT School. 'CRUD'

--- test_api_practice.py
+++ test_api_practice.py
@@ -1,6 +1,7 @@
 import requests
 import logging
 from assertpy import assert_that
+import pytest
 
 logger = logging.getLogger("Our_logger")
 # logger.setLevel(30)  # level of logging (10 it is debug), (20 it is warning)
@@ -12,7 +13,12 @@ response = requests.get(url_0).json()
 logger.warning(f"{response}")
 
 
-def test_get():
+@pytest.fixture(autouse=True, scope="module")
+def change_data():
+    return {}
+
+
+def test_get(change_data):
     response = requests.get(url_0)
     assert response.text == '{"users":"https://www.aqa.science/users/",' \
                             '"groups":"https://www.aqa.science/groups/"}'
@@ -22,7 +28,13 @@ def test_get():
 
     users_link = data["users"]
     group_link = data["groups"]
-
+    change_data['user_link'] = users_link
     print(users_link, group_link)
+
+
+def test_get_users(change_data):
+    user_link = change_data["user_link"]
+    response = requests.get(user_link)
+    print(response.json())
 
 
